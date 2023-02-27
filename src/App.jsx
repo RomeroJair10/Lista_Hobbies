92fee566076988eacbc0cbd66167fa79e80b50cd
Jair Romero Peña <jair@equipo3.com>
2023-02-27 14:50:01 -0600

    Lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name = "Jair Romero Peña";
+  return (
+    <di>
+      <h3>Lista de Hobbies de {name} </h3>
+      <ul>
+        <li>Tocar Piano</li>
+        <li>Ver Anime</li>
+        <li>Escuchar Musica</li>
+      </ul>
+    </di>
+  )
+}
+
+export default App
